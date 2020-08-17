bae378382c6f1387c9920f86e3107ef897c2e1a9
Michael Sjögren <15338875+Michael-Sjogren@users.noreply.github.com>
2020-08-17 12:00:29 +0200

    project cleanup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/products">Products</a></li>
+            <li><a href="/about">About us</a></li>
+            <li><a href="/login">login</a></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
